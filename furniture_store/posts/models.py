ee63a8a311e6038c9cafdc72695b2c02c1418735
Patrick Meehan <pjmeehanpj33@gmail.com>
2024-06-12 21:36:37 +0100

    added functionality for posts having authors, still need to make CRUD for users thought

--- furniture_store/posts/models.py
+++ furniture_store/posts/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    text = models.TextField()
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.user',
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
 
     def __str__(self):
-        return self.text[:50]
+        return self.title[:50]
