adb584a1756658542981febb6f43ffef02c5dd0c
Kitezh1 <156368541+Kitezh1@users.noreply.github.com>
2024-06-16 00:43:11 +0100

    added functionality for users to sign up, login, logout, including all their urls, views, templates & also settings. Also created registration folder in templates

--- furniture_store/furniture_store/settings.py
+++ furniture_store/furniture_store/settings.py
@@ -125,3 +125,6 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
--- furniture_store/furniture_store/urls.py
+++ furniture_store/furniture_store/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('posts/', include('posts.urls')),
     path('', include('shop.urls')),
