14ea49ce6f9383b52b269bb343745951c3036900
Patrick Meehan <pjmeehanpj@gmail.com>
2024-06-21 13:11:00 +0100

    testing git credentials

--- furniture_store/accounts/admin.py
+++ furniture_store/accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
-# Register your models here.
+# Register your models here..
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
