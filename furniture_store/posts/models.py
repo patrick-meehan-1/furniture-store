98bd9d4c58b844481cccaeea845fba347fc7bcaa
Patrick Meehan <pjmeehanpj33@gmail.com>
2024-06-27 09:54:56 +0100

    added permissions and authorization when it comes to needing to be logged in to view/create accounts. Also made it so only owner of artcile can edit/delete. User mixins for all these. Added new comment model so users can leave comments on posts.

--- furniture_store/posts/models.py
+++ furniture_store/posts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+
 
 # Create your models here.
 
@@ -24,3 +27,22 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name = 'comments',
+    )
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.comment
+    
+    def get_absolute_url(self):
+        return reverse('post_list')
+    
--- furniture_store/posts/views.py
+++ furniture_store/posts/views.py
@@ -1,29 +1,41 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 from django.urls import reverse_lazy
 
 
-class PostPageView(ListView):
+class PostPageView(LoginRequiredMixin, ListView):
     model = Post
     template_name = 'posts.html'
     context_object_name = "all_posts_list"
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_new.html'
-    fields = ['title', 'author', 'body', 'category']
+    fields = ['title', 'body', 'category']
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
 
-class PostDeleteView(DeleteView):
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,  DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('posts')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user
