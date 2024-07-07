0d1cf62335a511f4d587172b34024c7ef4f11983
Patrick Meehan <pjmeehanpj33@gmail.com>
2024-07-07 22:50:40 +0100

    added basic test case for creating users, will add test case later on in project - refeer back to lab 9 part 7

--- furniture_store/accounts/tests.py
+++ furniture_store/accounts/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import *
 
 # Create your tests here.
+
+class CustomUserTestCase(TestCase):
+    # Create custom user for testing
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="testuser@t.com",
+            password="tpassword",
+            age=23
+        )
+
+    def test_create_user(self):
+        # Test if a user is created properly
+        self.assertEqual(self.user.username, "testuser")
+        self.assertEqual(self.user.email, "testuser@t.com")
+        self.assertEqual(self.user.age, 23)
+        self.assertTrue(self.user.check_password('tpassword'))
+
+    def test_superuser_create_user(self):
+        # Test creating a superuser
+        self.superuser = get_user_model().objects.create_superuser(
+            username="suser",
+            email="suser@s.com",
+            password='spassword'
+        )
+        self.assertEqual(self.superuser.username, "suser")
+        self.assertEqual(self.superuser.email, "suser@s.com")
+        self.assertTrue(self.superuser.check_password, "spassword")
+        self.assertTrue(self.superuser.is_staff)
+        self.assertTrue(self.superuser.is_superuser)
