76a6e6aa9a1360767f2ddc6b562bd83399bd536e
Patrick Meehan <pjmeehanpj33@gmail.com>
2024-07-10 04:54:18 +0100

    added categories and products to shop app

--- furniture_store/shop/admin.py
+++ furniture_store/shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'description', 'category', 'stock',
+                    'available', 'created', 'updated']
+    list_editable = ['price', 'stock', 'available']
+    list_per_page = 20
+
+admin.site.register(Product, ProductAdmin)
