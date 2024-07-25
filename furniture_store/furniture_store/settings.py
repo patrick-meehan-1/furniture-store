2419726d08f512b0da57260fcad2852167f8f361
Patrick Meehan <pjmeehanpj33@gmail.com>
2024-07-25 02:23:30 +0100

    added image thumbnails

--- furniture_store/furniture_store/settings.py
+++ furniture_store/furniture_store/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'stripe',
+    'imagekit',
 ]
 
 MIDDLEWARE = [
--- furniture_store/shop/models.py
+++ furniture_store/shop/models.py
@@ -1,6 +1,8 @@
 import uuid
 from django.db import models
 from django.urls import reverse
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 
@@ -36,6 +38,10 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to = 'product', blank=True)
+    image_thumbnail = ImageSpecField(source='image',
+                                     processors=[ResizeToFill(250,250)],
+                                     format='JPEG',
+                                     options={'quality':100})
     stock = models.IntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, blank = True, null= True)
